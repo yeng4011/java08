15f4e4f70c1e9a7203e7d292a0dd09aeb8affa72
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2021-10-24 03:23:51 +0800

    Create class04.java

---
+++ class04.java
@@ -0,0 +1,18 @@
+class Cbox
+{
+    int length,width,height;
+}
+public class hw8_4
+{
+    public static void main(String arge[])
+    {
+        Cbox box;
+        box=new Cbox();
+        box.length=15;
+        box.width=10;
+        box.height=25;
+        System.out.println("length= "+box.length);
+        System.out.println("width= "+box.width);
+        System.out.println("height= "+box.height);
+    }
+}
